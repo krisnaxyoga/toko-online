70d3410e0018667c0f59bca5222ead593e381a7a
muliadev3 <softhouse3@themulia.com>
2024-12-24 11:40:38 +0800

    c

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +17,3 @@ use App\Http\Controllers\UploadController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/upload', [UploadController::class, 'upload'])->name('upload');
-Route::delete('/revert', [UploadController::class, 'revert'])->name('revert');
